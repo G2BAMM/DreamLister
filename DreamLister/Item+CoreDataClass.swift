eb087c5eb65cbed5440bad66ef064556c6c81259
G2BAM <brian"zungalow.com>
2017-07-10 17:43:05 +0100

    Removed timezone calculation and added getSummerTime function instead

--- DreamLister/Item+CoreDataClass.swift
+++ DreamLister/Item+CoreDataClass.swift
@@ -13,19 +13,6 @@ import CoreData
 public class Item: NSManagedObject {
     public override func awakeFromInsert() {
         super.awakeFromInsert()
-        let date = Date()
-        var summerTime: Date?
-        let tz = NSTimeZone.local
-        if tz.isDaylightSavingTime(for: date){
-            //We need to add an hour to our date as we're in BST (UK) or daylight savings elsewhere
-            summerTime = Calendar.current.date(byAdding:.hour, value: 1, to: Date())
-            print("Used daylight savings")
-        }else{
-            //No daylight savings in operation so just use the current date GMT (UK)
-            summerTime = Date()
-            print("Did not use daylight savings")
-        }
-
-        self.created = summerTime! as NSDate
+        self.created = getSummerTime(today: Date())
     }
 }
